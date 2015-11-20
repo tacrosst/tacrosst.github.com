dcfaedee25bb2cba1f5d7ad564bb7df7d193731f
tacrosst <tacrosst@wrw.kr>
2015-11-20 15:01:33 +0900

    메인 페이지 내용 추가

---
+++ _site/scripts/main.js
@@ -0,0 +1,31 @@
+ // var myHeading = document.querySelector('h1');
+ // myHeading.innerHTML = 'Hello world!';
+
+/*
+document.querySelector('html').onclick = function() {
+	alert('Ouch! stop poking me!');
+}
+*/
+
+
+
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
